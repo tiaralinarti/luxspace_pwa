12bdc3b5e4971f9bb358c21a52dfce62e6873566
Tiara Linarti <tiaralina3001@gmail.com>
2023-10-25 23:15:08 +0700

    migrasi template ke komponen

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
+import Browse from './components/Browse.js';
+import Arrived from './components/Arrived.js';
+import Clients from './components/Clients.js';
+import AsideMenu from './components/AsideMenu.js';
+import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Browse/>
+      <Arrived/>
+      <Clients/>
+      <AsideMenu/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
